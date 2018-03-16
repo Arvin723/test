686d78bf886f1a26b51f38586f20ef270573bea1
Arvin723 <1422529245@qq.com>
2018-03-17 00:20:18 +0800

    第一个.c文件
    first time create file

---
+++ spinmatrix.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+
+#define LEN 13
+
+void print(int (*a)[LEN] ,int len);
+void setValue(int num, int x, int y, int (*a)[LEN], int len);
+
+int main()
+{
+	int a[LEN][LEN] = {0};
+	setValue(1, 0, 0, a, LEN);
+	print(a, LEN);
+}
+
+void print(int (*a)[LEN] ,int len)
+{
+	for (int i = 0; i < LEN; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			printf("%-3d ", a[i][j]);
+		}
+	printf("\n");
+	}
+}
+
+void setValue(int num, int x, int y, int (*a)[LEN], int len)
+{
+	a[x][y] = num;
+	if (num == len*len)
+		return ;
+
+	if (y < len-1 && x == 0)
+		y++;
+	else if (y == len-1 && x < len-1)
+		x++;
+	else if (x == len-1 && y > 0)
+		y--;
+	else if (y == 0 && x > 0 && a[x-1][y] == 0)
+		x--;
+	else if (y ==0 && x == 1)
+		y++;
+	else
+	{
+		if (a[x][y+1] == 0)
+			y++;
+		else if (a[x+1][y] == 0)
+			x++;
+		else if (a[x][y-1] == 0)
+			y--;
+		else if (a[x-1][y] == 0)
+			x--;
+	}
+	num++;
+	setValue(num, x, y, a, len);
+}
